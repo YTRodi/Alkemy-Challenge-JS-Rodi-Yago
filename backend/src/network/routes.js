f81078df97b511fdd1b20743be7f33a2b6d3c73b
YTRodi <yagorodi@gmail.com>
2021-01-26 19:15:46 -0300

    add new middleware (JWT) for route /operation

--- backend/src/network/routes.js
+++ backend/src/network/routes.js
@@ -1,11 +1,14 @@
 const operationNetwork = require( '../components/operation/operationNetwork' );
 const userNetwork = require( '../components/user/userNetwork' );
 
+// Jwt
+const { checkToken } = require( '../auth/jwt' );
+
 const routes = ( server ) => {
 
     server.get( '/', ( req, res ) => res.send( 'Home' ) );
     server.use( '/user', userNetwork );
-    server.use( '/operation', operationNetwork );
+    server.use( '/operation', checkToken, operationNetwork );
 
 };
 
